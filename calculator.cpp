be7411596090ae1746087416792e0f78be5c4782
Magooch <crvallecito@gmail.com>
2025-07-18 05:11:11 -0600

    Add user input request for tip and print test output

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "Hello, world!\n";
+
+    int tip = 35;
+    std::cout << "Enter your tip: ";
+    std::cin >> tip;
+    std::cout << "Your tip is: " << tip << std::endl;
     return 0;
 }
