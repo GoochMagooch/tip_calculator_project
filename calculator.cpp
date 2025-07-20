cd1d6c5f5c096190fea520b4265006ed3ff6e5a5
Magooch <crvallecito@gmail.com>
2025-07-20 03:58:17 -0600

    Final active recall

---
+++ calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int tip;
+    
+    std::cout << "Enter your tip: \n";
+    std::cin >> tip;
+    std::cout << "Your tip is: $" << tip << std::endl;
+
+    return 0;
+}
