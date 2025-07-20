2aedd17dacac5f3276fba3068cb9961ce9fa4fea
Magooch <crvallecito@gmail.com>
2025-07-20 05:02:48 -0600

    Complete logic to request user input and calculate tip

--- calculator.cpp
+++ calculator.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main() {
-    int tip;
+    double total, tipPercentage;
     
-    std::cout << "Enter your tip: ";
-    std::cin >> tip;
-    std::cout << "Your tip is: $" << tip << std::endl;
+    std::cout << "Enter your total: ";
+    std::cin >> total;
+    std::cout << "Enter your tip percentage: ";
+    std::cin >> tipPercentage;
+    double tip = total * tipPercentage / 100;
+
+    std::cout << "Your tip will be: $" << std::fixed << std::setprecision(2) << tip << std::endl;
+    std::cout << "Your overall total will be: $" << std::fixed << std::setprecision(2) << total + tip << std::endl;
 
     return 0;
 }
